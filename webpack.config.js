d6ab243c6193e1d6f1665070045728b2af264693
amidav16 <davidamirth@gmail.com>
2020-09-05 17:07:41 +0200

    Webpage update

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
     filename: "bundle.js",
     path: path.resolve(__dirname, "public"),
   },
+  performance: {
+    hints: false,
+  },
   devServer: {
     contentBase: "./public",
     hot: true,
